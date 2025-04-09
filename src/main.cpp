fa6a03c6c4cf2b3d313e482d13b93cd8f33cf037
Elijah Durston-Rand <elijah@elijahdr.com>
2025-04-09 18:34:05 +0100

    1 million test

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ bool test_perfect_hashing(std::vector<std::string> &keys, HashFunction &hash_fun
     }
 }
 
-void run_recsplit_random_keys(int n=100000, uint32_t bucket_size=1000, uint32_t leaf_size=8) {
+void run_recsplit_random_keys(int n=1000000, uint32_t bucket_size=1000, uint32_t leaf_size=8) {
     std::vector<std::string> keys = generate_random_keys(n);
 
     RecSplit recsplit(bucket_size, leaf_size);
@@ -89,7 +89,7 @@ void run_recsplit_random_keys(int n=100000, uint32_t bucket_size=1000, uint32_t 
 }
 
 void run_sichash_random_keys() {
-    std::vector<std::string> keys = generate_random_keys(100000);
+    std::vector<std::string> keys = generate_random_keys(1000000);
 
     SicHash sichash(1000, 0.3, 0.3, 0.99);
     test_perfect_hashing(keys, sichash);
