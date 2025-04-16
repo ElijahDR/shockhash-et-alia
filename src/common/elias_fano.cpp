4b90dd13c5d3109484f26ec6acfe613ddbdcd576
Elijah Durston-Rand <elijah@elijahdr.com>
2025-04-16 15:27:46 +0100

    starting merge

--- src/common/elias_fano.cpp
+++ src/common/elias_fano.cpp
@@ -4,8 +4,8 @@ EliasFanoEncodedData elias_fano_encode(std::vector<uint32_t> data) {
     const int n = data.size();
     const uint32_t m = *data.rbegin() + 1;
 
-    int l = std::log2(m/n);
-    int r = std::log2(m) - l;
+    int l = std::floor(std::log2(m/n));
+    int r = std::ceil(std::log2(m)) - l; 
 
     std::vector<bool> lower;
     std::vector<bool> upper;
@@ -13,7 +13,7 @@ EliasFanoEncodedData elias_fano_encode(std::vector<uint32_t> data) {
     int prev = 0;
     for (auto x : data) {
         for (int i = l-1; i >= 0; i--) {
-            lower.push_back((1 << i) & x);
+            lower.push_back(((1 << i) & x) != 0);
         }
 
         int a = x >> l;
@@ -30,7 +30,7 @@ EliasFanoEncodedData elias_fano_encode(std::vector<uint32_t> data) {
 }
 
 std::vector<uint32_t> elias_fano_decode(EliasFanoEncodedData &data, uint32_t n) {
-    int l = std::log2(data.m/n);
+    int l = std::floor(std::log2(data.m/n));
     std::vector<uint32_t> data_decoded;
 
     uint32_t upper = 0, count = 0;
@@ -38,7 +38,7 @@ std::vector<uint32_t> elias_fano_decode(EliasFanoEncodedData &data, uint32_t n) 
         if (upper_bit) {
             uint32_t lower = 0;
             for (int j = 0; j < l; j++) {
-                lower |= data.lower[count * l + j];
+                lower |= (uint32_t)(data.lower[count * l + j]) << (l - j - 1);
             }
             data_decoded.push_back((upper << l) | lower);
             count += 1;
@@ -48,8 +48,4 @@ std::vector<uint32_t> elias_fano_decode(EliasFanoEncodedData &data, uint32_t n) 
     }
 
     return data_decoded;
-}
-
-uint32_t elias_fano_space(EliasFanoEncodedData &data) {
-    return (sizeof(data.lower) + sizeof(data.upper) + sizeof(data.m)) * 8 + data.upper.size() + data.lower.size();
 }
--- tests/common/elias_fano.test.cpp
+++ tests/common/elias_fano.test.cpp
@@ -6,6 +6,25 @@ TEST(EliasFanoTest, Encoding) {
     std::vector<uint32_t> data = {2,3,5,7,11,13,24};
     int n = data.size();
     EliasFanoEncodedData encoded = elias_fano_encode(data);
+    DEBUG_LOG(encoded.upper);
+    DEBUG_LOG(encoded.lower);
+    DEBUG_LOG(encoded.m);
+    std::vector<uint32_t> decoded_data = elias_fano_decode(encoded, n);
+    EXPECT_EQ(data, decoded_data);
+}
+
+TEST(EliasFanoTest, EncodingBig) {
+    std::vector<uint32_t> data = {};
+    for (int i = 0; i < 10; i++) {
+        data.push_back(1234 + i);
+    }
+    int n = data.size();
+    EliasFanoEncodedData encoded = elias_fano_encode(data);
+    DEBUG_LOG(encoded.upper);
+    DEBUG_LOG(encoded.upper.size());
+    DEBUG_LOG(encoded.lower);
+    DEBUG_LOG(encoded.lower.size());
+    DEBUG_LOG(encoded.m);
     std::vector<uint32_t> decoded_data = elias_fano_decode(encoded, n);
     EXPECT_EQ(data, decoded_data);
 }
