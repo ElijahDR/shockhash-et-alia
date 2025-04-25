d03b5e6ec0e928eae98e7d4da3a921998f0d63da
Elijah Durston-Rand <elijah@elijahdr.com>
2025-04-25 18:57:18 +0100

    update git ignore for tests

--- tests/CMakeFiles/run_test_golomb_rice.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_test_golomb_rice"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_test_golomb_rice.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- tests/CMakeFiles/test_broadword.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_broadword.
-# This may be replaced when dependencies are built.
